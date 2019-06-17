01ecc49a7a5b3fde544eb5f040e0a4c5361536e4
baiNan-git <692233099@qq.com>
2019-06-17 17:07:24 +0800

    插入tabs组件以及切换思路

--- day06/myapp/src/components/common/Prolist.1.js
+++ day06/myapp/src/components/common/Prolist.1.js
@@ -9,19 +9,19 @@ import { Link } from 'react-router-dom';
 //   </ul>
 // )
 
-// const Com = ({ prolist }) => (  //此处用了解构赋值和上面意思一样
-//   <ul className = 'prolist'>
-//     {  // 写了大括号就要写retuen返回值
-//       prolist.map(item => {
-//        return <li key = { item.id }>
-//         <Link to= {'/detail/' + item.id}>
-//        { item.title }
-//        </Link>
-//        </li>
-//       })
-//     }
-//   </ul>
-// )
+const Com = ({ prolist }) => (  //此处用了解构赋值和上面意思一样
+  <ul className = 'prolist'>
+    {  // 写了大括号就要写retuen返回值
+      prolist.map(item => {
+       return <li key = { item.id }>
+        <Link to= {'/detail/' + item.id}>
+       { item.title }
+       </Link>
+       </li>
+      })
+    }
+  </ul>
+)
 
 
 export default Com;
--- day06/myapp/src/views/Home.js
+++ day06/myapp/src/views/Home.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import Prolist from '@/components/common/Prolist';
 import Banner from '@/components/common/Banner';
+import { Tabs } from 'antd-mobile';
 
 class Com extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
       prolist: [],
-      bannerlist: []
+      bannerlist: [],
+      tabs: [
+        { title: '1st Tab'},
+        { title: '2st Tab'},
+        { title: '3st Tab'},
+        { title: '4st Tab'},
+        { title: '5st Tab'},
+        { title: '6st Tab'},
+        { title: '7st Tab'},
+        { title: '8st Tab'},
+        { title: '9st Tab'},
+      ],
+      activeIndex: 0
     }
   }
   componentDidMount () {
@@ -22,11 +35,35 @@ class Com extends React.Component {
       })
     })
   }
+  renderContent (tab) {
+    return (
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+        <p>Content of {tab.title}</p>
+      </div>
+    )
+    // let ta = ''
+    // this.state.activeIndex === 0 ? ta = <Com1 /> : ''
+    // this.state.activeIndex === 1 ? ta = <Com2 /> : ''
+    // this.state.activeIndex === 2 ? ta = <Com3 /> : ''
+    // this.state.activeIndex === 3 ? ta = <Com4 /> : ''
+    // this.state.activeIndex === 4 ? ta = <Com5 /> : ''
+    // return ta
+  }
+  ClickTab (tabs, index) {
+    console.log(index)
+    this.setState({
+      activeIndex: index
+    })
+  }
   render () {
     return(
       <div className="box">
         <header className="header">首页头部</header>
         <section className="content">
+          <Tabs tabs={this.state.tabs} onChange = {this.ClickTab.bind(this) } onTabClick = { this.ClickTab.bind(this) }
+          renderTabBar={props => <Tabs.DefaultTabBar {...props} page={3} />}>
+            {this.renderContent}
+          </Tabs>
           <Banner bannerlist = { this.state.bannerlist }/>
           <Prolist prolist = { this.state.prolist } { ...this.props }/>
           </section>
